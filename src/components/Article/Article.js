bb1cae875bf1122b6314978bf81dd61c2b126a7f
George Lemmon <george.lemmon@gmail.com>
2022-06-07 21:30:37 -0600

    Add styling to article cards

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,11 +1,12 @@
 import React from "react";
+import '../Article/Article.css'
 
 export const Article = ({article}) => {
   return (
-    <>
-      <h1>{article.title}</h1>
-      <h2>{article.byline}</h2>
-      <p>{article.abstract}</p>
-    </>
+    <div className="article">
+      <h1>{article.title || 'Unavaliable'}</h1>
+      <h2>{article.byline || 'Unavaliable'}</h2>
+      <p>{article.abstract || 'Unavaliable'}</p>
+    </div>
   )
 }
