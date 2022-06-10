52e9e67416ab6e15b53df4f4ed637e9fb45712e0
George Lemmon <george.lemmon@gmail.com>
2022-06-10 11:48:02 -0600

    Add detail to article detail

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import '../Article/Article.css'
 
-export const Article = ({ article }) => {
-  let url = `/${article.title}`
+export const Article = ({ id, article, setCurrentArticle }) => {
   return (
-    <Link to={url} style={{ textDecoration: 'none', color: '#000' }}>
-      <div className='article'>
-        <h1>{article.title || 'Unavaliable'}</h1>
-        <h2>{article.byline || 'Unavaliable'}</h2>
-        <p>{article.abstract || 'Unavaliable'}</p>
-      </div>
-    </Link>
+    <div
+      id={id}
+      className='article'
+      onClick={(e) => setCurrentArticle(article)}
+    >
+      <h1>{article.title || 'Unavaliable'}</h1>
+      <h2>{article.byline || 'Unavaliable'}</h2>
+      <p>{article.abstract || 'Unavaliable'}</p>
+    </div>
   )
 }
--- src/components/ArticleDetail/ArticleDetail.js
+++ src/components/ArticleDetail/ArticleDetail.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React from 'react'
+import '../ArticleDetail/ArticleDetail.css'
+// import { useParams } from 'react-router-dom'
 
-export const ArticleDetail = () => {
-  return <h1>Some Detail</h1>
+export const ArticleDetail = ({ article, setCurrentArticle }) => {
+  return (
+    <div className='detail'>
+      <h1>{article.title}</h1>
+      <h2>{article.byline}</h2>
+      <h3>{article.section}</h3>
+      <p>{article.abstract}</p>
+      <a href={article.url}>Go to article on NYTimes.com</a><br/><br/>
+      <button onClick={(e) => setCurrentArticle(null)}>
+        Click here to retun home
+      </button>
+    </div>
+  )
 }
