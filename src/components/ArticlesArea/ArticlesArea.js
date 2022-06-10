044a75538be2bd6094f4e44aef5ca82f2df1ec6b
George Lemmon <george.lemmon@gmail.com>
2022-06-10 11:47:39 -0600

    Add sort function to Articles area

--- src/components/ArticlesArea/ArticlesArea.js
+++ src/components/ArticlesArea/ArticlesArea.js
@@ -2,23 +2,69 @@ import React, { useState, useEffect } from 'react'
 import '../ArticlesArea/ArticlesArea.css'
 import { Article } from '../Article/Article'
 
-export const ArticlesArea = ({ errorMessage, articles }) => {
+export const ArticlesArea = ({
+  errorMessage,
+  articles,
+  setCurrentArticle,
+  sortCriteria
+}) => {
   const [articleCards, setArticleCards] = useState([])
+  const [sortedArticles, setSortedArticles] = useState([])
 
   useEffect(() => {
-    makeArticleCards()
+    sortArticles(articles, sortCriteria)
   }, [articles])
 
-  const makeArticleCards = () => {
-    let newArticles = articles.map((article) => {
-      return <Article key={article.title} article={article} />
+  useEffect(() => {
+    sortArticles(articles, sortCriteria)
+  }, [sortCriteria])
+
+  useEffect(() => {
+    makeArticleCards(sortedArticles)
+  }, [sortCriteria, sortedArticles])
+
+  const makeArticleCards = (sortedArticles) => {
+    let newArticles = sortedArticles.map((article) => {
+      return (
+        <Article
+          key={article.title}
+          id={article.uri.slice(15)}
+          article={article}
+          setCurrentArticle={setCurrentArticle}
+        />
+      )
     })
     setArticleCards(newArticles)
   }
+
+  const sortArticles = (articles, sortCriteria) => {
+    if (sortCriteria === 'date+') {
+      let newSort = sortByNewest(articles)
+      setSortedArticles(newSort)
+    } else if (sortCriteria === 'date-') {
+      let newSort = sortByOldest(articles)
+      setSortedArticles(newSort)
+    }
+  }
+
+  const sortByNewest = (articles) => {
+    let sortedArticlesDate = articles.sort((a, b) => {
+      return +new Date(a.published_date) - +new Date (b.published_date)
+    })
+    return sortedArticlesDate
+  }
+
+  const sortByOldest = (articles) => {
+    let sortedArticlesDate = articles.sort((a, b) => {
+      return +new Date(b.published_date) - +new Date(a.published_date)
+    })
+    return sortedArticlesDate
+  }
+
   return (
     <>
       {errorMessage && <section>errorMessage</section>}
-      {!errorMessage && <div className="article-area" >{articleCards}</div>}
+      {!errorMessage && <div className='article-area'>{articleCards}</div>}
     </>
   )
 }
