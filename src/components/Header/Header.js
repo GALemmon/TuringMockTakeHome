d63203967882bfb2df5291b9010126e1483624ed
George Lemmon <george.lemmon@gmail.com>
2022-06-10 11:46:10 -0600

    Add form to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,13 @@
-import React from "react";
+import React from 'react'
+import Form from '../Form/Form.js'
 
-export const Header = () => {
-  return <h2>Header</h2>
+export const Header = ({ setSortCriteria, sortCriteria, currentArticle }) => {
+  return (
+    <div className='header'>
+      <h1>NY Times Top Stories</h1>
+      {!currentArticle && (
+        <Form setSortCriteria={setSortCriteria} sortCriteria={sortCriteria} />
+      )}
+    </div>
+  )
 }
